76a53dcb51fe46b205185172a5ac563051191881
kanxuehe <K_xuehe@163.com>
2025-10-25 23:41:12 +0800

    u

---
+++ pastPaper/ib-math/analysis-and-approaches-sl/ids.js
@@ -0,0 +1,31 @@
+
+[
+  '07210af4-4d76-45ea-9481-054f7d7b62ff',
+  'abc48865-cc79-4cc6-85c2-3794809906bf',
+  'c8109bd0-2672-4fc1-b4b1-dd80b8da8167',
+  'cb547d2d-2871-4550-b9e2-785b05c36cf8',
+  '296e9fb6-dbd3-4e54-ae17-a18d67eca965',
+  'b29be8d9-17fc-49d7-8f00-7dbd79f5c5cf',
+  '616b8212-d976-4b61-8523-a84b2f935f9c',
+  '404c0297-d58b-43fe-8e32-14407b26a4c5',
+  'f217335e-9dad-4379-a8e2-250dbcb2a61b',
+  '22e73cd9-f787-478a-b647-6bc8ee453e8a',
+  'dc9447f2-e644-4c9a-990d-70167d9d2904',
+  '448f28e1-bd90-4776-8a02-14118ae94f37',
+  'ee0c105b-707a-4665-af06-6b262a3f885c',
+  'd77fcbd9-fddc-4ac8-956a-a8e203b7e518',
+  'd3b86a32-fa08-4f2d-bc82-4d9dd5fa379a',
+  'da3c1220-f555-454e-a0ae-08b15f0e1bd8',
+  'da9ca04a-0390-4b78-8217-32fd0cbd8c13',
+  'e278d127-e255-405b-b72d-4331d569ecdd',
+  'd85a4f20-876c-4b8b-9fc4-09412b1b5cdc',
+  '0f6cf628-056d-45e8-b4a9-e88281bec11e',
+  '9f20a5a2-b8ad-4dd2-90fa-c37f7973f215',
+  '4a7b8eb1-b41c-4ad2-b009-0e8d8f6e7d52'
+];
+
+// const result = [];
+// list.map((item) => {
+//   const ids = item.papers.map((paper) => paper.id);
+//   result.push(...ids);
+// });
